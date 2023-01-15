58a1ddd977b0713d5289cd05ada7237ab9c88891
Alvaro Prado <alvarojpt04.ws@gmail.com>
2023-01-15 13:28:02 -0500

    Fusion del menú en mobile

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 let user = document.querySelector('.navbar-email')
 let desktopMenu = document.querySelector('.desktop-menu')
-
+let menuMobile = document.querySelector('.menu-mobile')
+let iconMenu = document.querySelector('.menu') 
 
 user.addEventListener('click', toggleDesktopMenu) //Este toggleDesktopMenu solo es el nombre de una función
+
+iconMenu.addEventListener('click', toggleMenuMobile)
 
 function toggleDesktopMenu(){
 
 desktopMenu.classList.toggle('inactive')
 
+}
+
+
+function toggleMenuMobile(){
+
+menuMobile.classList.toggle('inactive')
 }
    
